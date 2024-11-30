c9d3ae73e64fa1ee66075c630f4eeefb8613415b
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2024-11-30 22:45:47 +0200

    Update settings.py
    debug true

--- myWorldSite/myWorldSite/settings.py
+++ myWorldSite/myWorldSite/settings.py
@@ -32,7 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://radiant-brushlands-79772-293c5a123503.herokuapp.com", "http://127.0.0.1:8000", "127.0.0.1"]
 
