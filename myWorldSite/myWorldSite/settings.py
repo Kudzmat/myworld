b8b694cd7b5e5a4b269cfbdf0e60b70cc0799c8d
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2024-12-05 00:19:40 +0200

    Update settings.py
    added https://www.kudzayibamhare.com to allowed hosts

--- myWorldSite/myWorldSite/settings.py
+++ myWorldSite/myWorldSite/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://radiant-brushlands-79772-293c5a123503.herokuapp.com", "http://127.0.0.1:8000", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://radiant-brushlands-79772-293c5a123503.herokuapp.com", "https://www.kudzayibamhare.com", "http://127.0.0.1:8000", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ["https://radiant-brushlands-79772-293c5a123503.herokuapp.com", "http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = ["https://radiant-brushlands-79772-293c5a123503.herokuapp.com", "https://www.kudzayibamhare.com", "http://127.0.0.1:8000"]
 
 
 # Application definition
