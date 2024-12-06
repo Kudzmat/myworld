a4be30dbbace354796ad5c77c991e11bbcd4d648
Kudzayi <matbamhare@gmail.com>
2024-12-06 09:09:25 +0200

    added amazon s3 bucket settings and new requirements

--- myWorldSite/myWorldSite/settings.py
+++ myWorldSite/myWorldSite/settings.py
@@ -139,7 +139,20 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
+INSTALLED_APPS += ["storages"]
+
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "my-django-media"  # Replace with your bucket name
+AWS_S3_REGION_NAME = "your-region"  # e.g., "us-east-1"
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+# Media files
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
